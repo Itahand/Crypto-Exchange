a7a69fb06c9943ab6ffbb6a3c2b52fbb7b464559
Itahand Naizir <itahand.naizir@gmail.com>
2022-07-20 13:21:54 -0500

    Added functionality to make Orders on the exchange.

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -7,10 +7,40 @@ import "./Token.sol";
 contract Exchange {
     address public feeAccount;
     uint256 public feePercent;
+    uint256 public ordersCount;
+
+    // Tokens mapping
     mapping(address => mapping(address => uint256)) public tokens;
+
+    // Orders mapping
+    mapping(uint256 => _Order) public orders;
+
+    // Events
 
     event Deposit(address token, address user, uint256 amount, uint256 balance);
     event Withdraw(address token, address user, uint256 amount, uint256 balance);
+    event Order(
+      uint256 id,
+      address user,
+      address tokenGet,
+      uint256 amountGet,
+      address tokenGive,
+      uint256 amountGive,
+      uint256 timestamp
+    );
+
+    // Orders database
+
+    struct _Order {
+      // Attributes of an order
+      uint256 id; // Unique identifier for order
+      address user; // Who made the order
+      address tokenGet;
+      uint256 amountGet;
+      address tokenGive;
+      uint256 amountGive;
+      uint256 timestamp;
+    }
 
     constructor(address _feeAccount, uint256 _feePercent) {
         feeAccount = _feeAccount;
@@ -35,13 +65,9 @@ contract Exchange {
     function withdrawToken(address _token, uint256 _amount)
     public
     {
-      // Ensure user has enough tokens to withdraw
       require(tokens[_token][msg.sender] >= _amount);
-      // Transfer tokens to the user
       Token(_token).transfer(msg.sender, _amount);
-      // Update user balance
         tokens[_token][msg.sender] = tokens[_token][msg.sender] - _amount;
-      // Emit event
         emit Withdraw(_token, msg.sender, _amount, tokens[_token][msg.sender]);
 
     }
@@ -53,5 +79,31 @@ contract Exchange {
     {
         return tokens[_token][_user];
     }
+
+    // Make and Cancel orders.
+
+    function makeOrder(
+      address _tokenGet,
+      uint256 _amountGet,
+      address _tokenGive,
+      uint256 _amountGive
+      ) public
+      {
+        // Cancel if the token isn't in the exchange
+        require(balanceOf(_tokenGive, msg.sender) >= _amountGive);
+
+        // Make the order
+        ordersCount = ordersCount + 1;
+        orders[ordersCount] = _Order(
+          ordersCount, // id
+          msg.sender, // user
+          _tokenGet,
+          _amountGet,
+          _tokenGive,
+          _amountGive,
+          block.timestamp
+        );
+        emit Order(ordersCount, msg.sender, _tokenGet, _amountGet, _tokenGive, _amountGive, block.timestamp);
+      }
 
 }
