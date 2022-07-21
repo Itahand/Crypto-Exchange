95987cdd522b723b2f7a49962d6c455afc26b0ed
Itahand Naizir <itahand.naizir@gmail.com>
2022-07-21 10:36:42 -0500

    Added market selection.

--- src/components/App.js
+++ src/components/App.js
@@ -11,6 +11,7 @@ import {
 } from '../store/interactions';
 
 import Navbar from './Navbar'
+import Markets from './Markets'
 
 function App() {
   const dispatch = useDispatch()
@@ -22,8 +23,15 @@ function App() {
     // Fetch current network's chainId (e.g. hardhat: 31337, kovan: 42)
     const chainId = await loadNetwork(provider, dispatch)
 
-    // Fetch current account & balance from Metamask
-    await loadAccount(provider, dispatch)
+    // Reload page when network changes
+    window.ethereum.on('chainChanged', () => {
+      window.location.reload()
+    })
+
+    // Fetch current account & balance from Metamask when changed
+    window.ethereum.on('accountsChanged', () => {
+      loadAccount(provider, dispatch)
+    })
 
     // Load token smart contracts
     const NOAH = config[chainId].NOAH
@@ -43,12 +51,12 @@ function App() {
   return (
     <div>
 
-      <Navbar/>
+      <Navbar />
 
       <main className='exchange grid'>
         <section className='exchange__section--left grid'>
 
-          {/* Markets */}
+          <Markets/>
 
           {/* Balance */}
 
